44dccb98a831c8a7b43cf43b016f16c80d7a38b4
David Cha <dc136072@gmail.com>
2024-07-23 17:28:53 -0500

    Update Views, Urls, and Templates
    Setting up the Views, Urls, and Templates

--- CHATGPT/chatapp/views.py
+++ CHATGPT/chatapp/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    context = {}
+    return render(request, 'chatapp/index.html', context)
+
+def signIn(request):
+    context = {}
+    return render(request, 'chatapp/signIn.html', context)
+
+def signUp(request):
+    context = {}
+    return render(request, 'chatapp/signUp.html', context)
