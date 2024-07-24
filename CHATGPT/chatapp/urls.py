320df3f4d88ea51369c2356cbda91c6d40f5c869
David Cha <dc136072@gmail.com>
2024-07-24 11:52:41 -0500

    Add Authentication and Users

--- CHATGPT/chatapp/urls.py
+++ CHATGPT/chatapp/urls.py
@@ -3,6 +3,6 @@ from .import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('login', views.login, name='login'),
-    path('signUp', views.signUp, name='signUp')
+    path('login', views.login_view, name='login'),
+    path('signUp', views.signUp, name='signUp'),
 ]
--- CHATGPT/chatapp/views.py
+++ CHATGPT/chatapp/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login as login_view, logout
+from .forms import UserForm
+
 
 # Create your views here.
 def index(request):
@@ -10,5 +13,16 @@ def login(request):
     return render(request, 'chatapp/login.html', context)
 
 def signUp(request):
-    context = {}
+    form = UserForm()
+    if request.method == 'POST':
+        form = UserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = request.POST['username']
+            password = request.POST['password1']
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login_view(request, user)
+                return redirect('index')
+    context = {'form': form}
     return render(request, 'chatapp/signUp.html', context)
